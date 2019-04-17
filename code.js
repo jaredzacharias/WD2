a93346d6ebda9bbd35de6af07e1b418d875fdebe
jaredzacharias <47986719+jaredzacharias@users.noreply.github.com>
2019-04-17 13:55:39 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
       $('.toggleNav').click(function() {
         $('#flex-nav ul').toggleClass('open');
+      $('.button').click(DropShadow) {
+            $(.phone).click(show);
+      $('.phone').click(show) {
+            $(.phone).click(disappear);
       });
     });
